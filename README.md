e823a79200ad39e257bc47fd4deb3483332418e1
Lorival Chapuis <lorival.ecp@gmail.com>
2021-09-07 17:59:07 +0100

    chore(configuration): added keyboard configuration

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 
 This repository install packages and customize Debian to suit me.
 
-Just run install.sh
+Just run ```install.sh```
 
 ## Packages
 - Docker Engine
 - Visual Studio Code
 - Git with Git-GUI
+
+## Configurations
+- Keyboard
--- install.sh
+++ install.sh
@@ -62,6 +62,14 @@ sudo apt-get autoremove
 sudo apt-get clean
 echo
 
+echo "-----------------------"
+echo "Configuring keyboard..."
+echo "-----------------------"
+echo 
+export PATH=$PATH:/usr/sbin
+sudo dpkg-reconfigure keyboard-configuration
+echo
+
 echo "----------------------"
 echo "Done! enjoy it... >:-)"
 echo "----------------------"
